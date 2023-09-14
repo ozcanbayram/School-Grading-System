bf8dda7244fe53163ea51db2885bad0e6786d5d6
Özcan Bayram <117665864+ozcanbayram@users.noreply.github.com>
2023-09-14 22:48:49 +0300

    All debugging is complated.

--- OgrenciNotSistemi/FrmOgretmenDetay.cs
+++ OgrenciNotSistemi/FrmOgretmenDetay.cs
@@ -29,29 +29,53 @@ namespace OgrenciNotSistemi
 
 
 
-        private void btbKaydet_Click(object sender, EventArgs e)
+        private void btbKaydet_Click(object sender, EventArgs e) // All debugging is complated.
         {
-            baglanti.Open();
-            SqlCommand komut = new SqlCommand("insert into Tbl_Ders (OGRNUMARA,OGRAD,OGRSOYAD) values (@p1,@p2,@p3)", baglanti);
-            komut.Parameters.AddWithValue("@p1", mskNo.Text);
-            komut.Parameters.AddWithValue("@p2", txtAd.Text);
-            komut.Parameters.AddWithValue("@p3", txtSoyad.Text);
-            komut.ExecuteNonQuery();
-            baglanti.Close();
-            MessageBox.Show("Öğrenci sisteme kaydedildi.");
-            this.tbl_DersTableAdapter.Fill(this.dbNotKayitDataSet.Tbl_Ders);
+            
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("insert into Tbl_Ders (OGRNUMARA,OGRAD,OGRSOYAD) values (@p1,@p2,@p3)", baglanti);
+                komut.Parameters.AddWithValue("@p1", mskNo.Text);
+                komut.Parameters.AddWithValue("@p2", txtAd.Text);
+                komut.Parameters.AddWithValue("@p3", txtSoyad.Text);
+
+                if (mskNo.Text == "" || txtAd.Text == "" || txtSoyad.Text == "")
+                {
+                    MessageBox.Show("Lütfen eklemek istediğiniz öğrencinin bilgilerini eksiksiz giriniz.");
+                }
+
+                else
+                {
+                    komut.ExecuteNonQuery();
+                    baglanti.Close();
+                    MessageBox.Show("Öğrenci sisteme kaydedildi.");
+                    this.tbl_DersTableAdapter.Fill(this.dbNotKayitDataSet.Tbl_Ders);
+                }
+            
+            
+
+
 
         }
 
-        private void btnSil_Click(object sender, EventArgs e)
+        private void btnSil_Click(object sender, EventArgs e) //All debugging is complated.
         {
             baglanti.Open();
             SqlCommand komut = new SqlCommand("delete from Tbl_ders where OGRNUMARA=@p1", baglanti);
             komut.Parameters.AddWithValue("@p1", mskNo.Text);
-            komut.ExecuteNonQuery();
-            baglanti.Close();
-            MessageBox.Show("Öğrenci sistemden silindi.");
-            this.tbl_DersTableAdapter.Fill(this.dbNotKayitDataSet.Tbl_Ders);
+            if (mskNo.Text == "" || txtAd.Text == "" || txtSoyad.Text == "")
+            {
+                MessageBox.Show("Lütfen silmek istediğiniz öğrenciyi seçiniz ya da id numarasını giriniz.");
+            }
+
+            else
+            {
+                komut.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Öğrenci sistemden silindi.");
+                this.tbl_DersTableAdapter.Fill(this.dbNotKayitDataSet.Tbl_Ders);
+            }
+
+
 
         }
 
@@ -66,49 +90,56 @@ namespace OgrenciNotSistemi
 
         private void btnGuncelle_Click(object sender, EventArgs e)
         {
-            try
+
+            double ortalama, s1, s2, s3;
+
+            string durum;
+            bool durumSonucu;
+
+            s1 = Convert.ToDouble(txtS1.Text);
+            s2 = Convert.ToDouble(txtS2.Text);
+            s3 = Convert.ToDouble(txtS3.Text);
+            ortalama = (s1 + s2 + s3) / 3;
+            lblOrtalama.Text = ortalama.ToString("00.00");
+
+            if (ortalama >= 50)
             {
-                double ortalama, s1, s2, s3;
-
-                string durum;
-                bool durumSonucu;
-
-                s1 = Convert.ToDouble(txtS1.Text);
-                s2 = Convert.ToDouble(txtS2.Text);
-                s3 = Convert.ToDouble(txtS3.Text);
-                ortalama = (s1 + s2 + s3) / 3;
-                lblOrtalama.Text = ortalama.ToString("00.00");
-
-                if (ortalama >= 50)
-                {
-                    lblDurum.Text = "GEÇTİ";
-                    durumSonucu = true;
-                }
-                else
-                {
-                    lblDurum.Text = "KALDI";
-                    durumSonucu = false;
-                }
+                lblDurum.Text = "GEÇTİ";
+                durumSonucu = true;
+            }
+            else
+            {
+                lblDurum.Text = "KALDI";
+                durumSonucu = false;
+            }
 
 
-                baglanti.Open();
-                SqlCommand komut = new SqlCommand("Update Tbl_Ders Set S1=@p1, S2=@p2, S3=@p3, ORTALAMA=@p5, DURUM=@p6 where OGRNUMARA=@p4", baglanti);
-                komut.Parameters.AddWithValue("@p1", txtS1.Text);
-                komut.Parameters.AddWithValue("@p2", txtS2.Text);
-                komut.Parameters.AddWithValue("@p3", txtS3.Text);
-                komut.Parameters.AddWithValue("@p4", mskNo.Text);
-                komut.Parameters.AddWithValue("@p5", decimal.Parse(lblOrtalama.Text));
-                komut.Parameters.AddWithValue("@p6", (durumSonucu));
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Update Tbl_Ders Set S1=@p1, S2=@p2, S3=@p3, ORTALAMA=@p5, DURUM=@p6 where OGRNUMARA=@p4", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtS1.Text);
+            komut.Parameters.AddWithValue("@p2", txtS2.Text);
+            komut.Parameters.AddWithValue("@p3", txtS3.Text);
+            komut.Parameters.AddWithValue("@p4", mskNo.Text);
+            komut.Parameters.AddWithValue("@p5", decimal.Parse(lblOrtalama.Text));
+            komut.Parameters.AddWithValue("@p6", (durumSonucu));
+
+            if (txtS1.Text == "" || txtS2.Text == "" || txtS3.Text == "" || mskNo.Text == "")
+            {
+                MessageBox.Show("Lütfen bilgilerini güncellemek istediğiniz öğrenciyi seçiniz ya da numarasını giriniz.");
+            }
+
+            else
+            {
                 komut.ExecuteNonQuery();
                 baglanti.Close();
                 MessageBox.Show("Öğrenci notları güncellendi.");
                 this.tbl_DersTableAdapter.Fill(this.dbNotKayitDataSet.Tbl_Ders);
-            }catch(Exception)
-            {
-                MessageBox.Show("Lütfen geçerli değerler girdiğinizden emin olun.");
             }
 
-            
+
+
+
+
 
         }
 
